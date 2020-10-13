386c8562d29e50edbecaf924ff024ed33367364b
Laura Rubio <laurarubio11@hotmail.com>
2020-10-13 18:47:12 -0500

    view dependencies fix

--- vueapp/src/Data/dependencies.js
+++ vueapp/src/Data/dependencies.js
@@ -1,4 +1,17 @@
 export const Dependencies = [
+    {
+        nombre: 'Pepita',
+        id: 1
+    },
+    {
+        nombre: 'Marta',
+        id: 2
+
+    },
+    {
+        nombre: 'Pedro',
+        id: 3
+    },
 
     
 ]

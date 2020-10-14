c71b5c25d7b23aaa0414f08721bd5c0ae2dec6b0
Laura Rubio <laurarubio11@hotmail.com>
2020-10-13 19:58:58 -0500

    merge with delete dependencies

---
+++ vueapp/src/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+import {Dependencies} from './Data/dependencies.js'
+export default new Vuex.Store({
+    state:{
+        dependencies: Dependencies,       
+    },
+    mutations:{
+        deleteDependency(state, payload){            
+            state.dependencies.splice(payload,1)
+        },
+
+    },
+    actions:{
+            deleteDependency({commit},payload){
+                //console.log(this.state.dependencies[payload].name)
+                commit('deleteDependency',payload)
+
+            },
+    }
+})

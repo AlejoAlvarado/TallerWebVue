fb074815e47df76ae883106530557f66974c3d94
Alejandro Alvarado <Nuestrocanal1@gmail.com>
2020-10-19 15:25:24 -0500

    deleted search bar in home

--- vueapp/src/components/Home.vue
+++ vueapp/src/components/Home.vue
@@ -1,29 +1,20 @@
 <template>
   <v-container>
-    
-    <v-card elevation="10"  shaped  >
+    <v-card elevation="10" shaped>
       <v-card-title>Bienvenido</v-card-title>
       <v-card-text>
         <p>
-           Aquí podras realizar toda la gestión de los usuarios y sus dependencias con facilidad:
-          En la pestaña de usuario podrás agregar nuevos usuarios, en la pestaña de dependecia podras agregar nuevas dependencias.          
+          Aquí podras realizar toda la gestión de los usuarios y sus
+          dependencias con facilidad: En la pestaña de usuario podrás agregar
+          nuevos usuarios, en la pestaña de dependecia podras agregar nuevas
+          dependencias.
         </p>
         <p>
-          Para visualizar los datos que has agregado puedes verlo en Lista de usuarios o Lista de dependencias según desees.
+          Para visualizar los datos que has agregado puedes verlo en Lista de
+          usuarios o Lista de dependencias según desees.
         </p>
-        
       </v-card-text>
     </v-card>
-    
-    <v-toolbar class="mt-10">
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-text-field> </v-text-field>
-      <v-btn :items="items" icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
   </v-container>
 </template>
 
